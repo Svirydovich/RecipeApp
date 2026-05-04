e9375dc24d9646281ff4e3da1bcbfbb08aa4ff6d
Svirydovich <standup.minsk@gmail.com>
2026-05-04 23:55:07 +0300

    RA-11 Update code after the second review

--- app/src/main/java/com/example/englishwordsapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/englishwordsapp/CategoriesListFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.englishwordsapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
