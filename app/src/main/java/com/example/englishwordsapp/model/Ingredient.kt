284ef44ecce31621cefb71eb3b7f93fae39b60d5
Svirydovich <standup.minsk@gmail.com>
2026-05-06 07:16:04 +0300

    RA-12 Implement the Parcelable interface and pass a Recipe object

--- app/src/main/java/com/example/englishwordsapp/model/Ingredient.kt
+++ app/src/main/java/com/example/englishwordsapp/model/Ingredient.kt
@@ -1,8 +1,12 @@
 package com.example.englishwordsapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Ingredient(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String
-)
+) : Parcelable
 
