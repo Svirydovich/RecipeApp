7306ae9b84e2aade8e4cc01d39e8e0776fa0a663
Svirydovich <standup.minsk@gmail.com>
2026-05-12 03:07:55 +0300

    RA-14 Add SeekBar to calculate ingredients

--- app/src/main/java/com/example/englishwordsapp/IngredientsAdapter.kt
+++ app/src/main/java/com/example/englishwordsapp/IngredientsAdapter.kt
@@ -10,24 +10,43 @@ import com.example.englishwordsapp.model.Ingredient
 class IngredientsAdapter(private val ingredients: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.IngredientViewHolder>() {
 
+    private var quantity = 1
+
     class IngredientViewHolder(private val binding: ItemIngredientBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         @SuppressLint("SetTextI18n")
-        fun bind(ingredient: Ingredient) {
+        fun bind(ingredient: Ingredient, quantity: Int) {
             binding.tvIngredientName.text = ingredient.description
-            binding.tvIngredientQuantity.text = "${ingredient.quantity} ${ingredient.unitOfMeasure}"
+
+            val baseQuantity = ingredient.quantity.toDoubleOrNull() ?: 0.0
+
+            val totalQuantity = baseQuantity * quantity
+
+            val quantityText = if (totalQuantity == 0.0) {
+                ingredient.quantity
+            } else if (totalQuantity % 1 == 0.0) {
+                totalQuantity.toInt().toString()
+            } else String.format("%.1f", totalQuantity)
+
+            binding.tvIngredientQuantity.text = "$quantityText ${ingredient.unitOfMeasure}"
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientViewHolder {
-        val binding = ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return IngredientViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: IngredientViewHolder, position: Int) {
-        holder.bind(ingredients[position])
+        holder.bind(ingredients[position], quantity)
     }
 
     override fun getItemCount(): Int = ingredients.size
+
+    fun updateIngredients(newQuantity: Int) {
+        quantity = newQuantity
+        notifyDataSetChanged()
+    }
 }
