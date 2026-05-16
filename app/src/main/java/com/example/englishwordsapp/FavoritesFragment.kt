1c36c4d6aa7a09ccde7ab70dcbae4365f3b90d7b
Svirydovich <standup.minsk@gmail.com>
2026-05-16 23:14:47 +0300

    RA-18 Implement the Favorites screen

--- app/src/main/java/com/example/englishwordsapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/englishwordsapp/FavoritesFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.englishwordsapp.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
@@ -25,4 +28,49 @@ class FavoritesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun openRecipeByRecipeId(recipeId: Int) {
+        val recipe = STUB.getRecipeById(recipeId)
+
+        if (recipe != null) {
+            val bundle = Bundle().apply {
+                putParcelable(RecipeFragment.ARG_RECIPE, recipe)
+            }
+
+            parentFragmentManager.commit {
+                replace<RecipeFragment>(R.id.mainContainer, args = bundle)
+                addToBackStack(null)
+            }
+        }
+    }
+
+    private fun initRecycler() {
+        val favoriteIds = STUB.getFavorites(requireContext())
+
+        val favoriteRecipes = STUB.getRecipesByIds(favoriteIds)
+
+        val recipeAdapter = RecipeAdapter(favoriteRecipes, { recipeId ->
+            openRecipeByRecipeId(recipeId)
+        }, requireContext())
+
+        binding.rvFavorites.apply {
+            adapter = recipeAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        if (favoriteRecipes.isEmpty()) {
+            binding.rvFavorites.visibility = View.GONE
+            binding.tvEmptyState.visibility = View.VISIBLE
+        } else {
+            binding.rvFavorites.visibility = View.VISIBLE
+            binding.tvEmptyState.visibility = View.GONE
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecycler()
+    }
+
 }
