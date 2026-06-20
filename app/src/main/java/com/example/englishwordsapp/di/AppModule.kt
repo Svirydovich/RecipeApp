6b5991d21f56057eb63c875c4310cc41e1000359
Svirydovich <standup.minsk@gmail.com>
2026-06-20 15:31:19 +0300

    RA-45 Update code after review

--- app/src/main/java/com/example/englishwordsapp/di/AppModule.kt
+++ app/src/main/java/com/example/englishwordsapp/di/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,7 +16,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class AppModule() {
+class AppModule {
     @Provides
     @Singleton
     fun provideLoggingInterceptor(): HttpLoggingInterceptor {
@@ -24,8 +25,17 @@ class AppModule() {
 
     @Provides
     @Singleton
-    fun provideApiService(): RecipeApiService = Retrofit.Builder()
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(okHttpClient: OkHttpClient): RecipeApiService = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(RecipeApiService::class.java)
